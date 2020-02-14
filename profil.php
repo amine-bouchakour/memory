1a4c94896d126d27e49d99ec06f2cde76fd65916
Bouchakour <amine.bouchakour@laplateforme.io>
2020-02-14 15:51:34 +0100

    travail sur calcul et addition de deux tableau (besttime et besttentative) pour donner tableau bestscore

--- profil.php
+++ profil.php
@@ -1,7 +1,8 @@
 <html>
-<title>Wall of Fame</title>
+<title>Profil</title>
 <link rel="stylesheet" href="walloffame.css">
 </html>
+
 <?php
 session_start();
 
@@ -21,15 +22,16 @@ class scoreprofil{
 <br>
 <!-- TEST VOS PARTIE -->
 <?php
-
-echo $_SESSION['id'].'<br/>';
-echo $_SESSION['login'].'<br/>';
+if(isset($_SESSION['login']) and isset($_SESSION['id'])){
+    echo 'id= '.$_SESSION['id'].'<br/>';
+    echo 'login= '.$_SESSION['login'].'<br/>';
+}
 
 
 $n=1;
 ?> <table>
     <th>TOP 10</th>
-    <th>---VOS PARTIE---</th>
+    <th>---VOS PARTIES---</th>
     <th>
         <ul id="menu-accordeon">
             <li><a href="#"><?php if(!isset($levelbis)){ echo 'Level';}else{echo 'Level '.$_GET['levelbis'];} ?></a>
@@ -75,26 +77,71 @@ $levelbis=5;
 
 $connexion=mysqli_connect('localhost','root','','memory');
 
+// REQUETE POINTS BESTTIME
 $requete1="SELECT points FROM besttime WHERE level='".$levelbis."' and login='".$_SESSION['login']."' ORDER BY points DESC";
 $query=mysqli_query($connexion,$requete1);
 $resultatlevel1=mysqli_fetch_all($query);
-echo $resultatlevel1[0];
-var_dump($resultatlevel1);
+$m=0;
+$ptstime=0;
+if(!empty($resultatlevel1)){
+    while($m<count($resultatlevel1)){
+        $ptstime=$ptstime+$resultatlevel1[$m][0];
+        ++$m;
+    }
+}
+else{
+    $ptstime=0;
+}
 
+
+// REQUETE POINTS BESTTENTATIVE
 $requete1="SELECT points FROM besttentative WHERE level='".$levelbis."' and login='".$_SESSION['login']."' ORDER BY points DESC";
 $query1=mysqli_query($connexion,$requete1);
 $resultatlevel2=mysqli_fetch_all($query1);
-echo $resultatlevel2[0];
-var_dump($resultatlevel2);
+$p=0;
+if(!empty($resultatlevel2)){
+    while($p<count($resultatlevel2)){
+        $ptstentative=$ptstentative=$resultatlevel1[$p][0];
+        ++$p;
+    }
+    $ptstentative=$resultatlevel2[0][0];
+}
+else{
+    $ptstentative=0;
+}
 
+// BESTTENTATIVE + BESTTIME POUR TABLEAU SCORETOTAL
+if(isset($ptstentative) and isset($ptstime)){
+    $ptstotal=$ptstime+$ptstentative;
+    $connexion=mysqli_connect("Localhost",'root','','memory');
+    $requete0="SELECT points FROM bestscore WHERE login='".$_SESSION['login']."'";
+    $query0=mysqli_query($connexion,$requete0);
+    $resultat0=mysqli_fetch_all($query0);
+    echo $resultat0[0][0];
+    $ptstotalstocké=$resultat0[0][0];
+    $ptstotal=$ptstotal+$ptstotalstocké;
+    
+    $requete="UPDATE bestscore SET points='".$ptstotal."' WHERE login='".$_SESSION['login']."' ";
+    $query=mysqli_query($requete);
+    echo ($requete);
+
+}
+
+echo 'Points time : '.$ptstime.'<br/>';
+echo 'Points tentative '.$ptstentative.'<br/>';
+echo 'Points Totals : '.$ptstotal.'<br/>';
+
+
+
+++$n;
 $k=0;
 while ($n<=10){    
 // $resultatlevel2[0][0];//Login
 // $resultatlevel2[0][1];//tentative
 // $resultatlevel2[0][2];//points
 while($k<count($resultatlevel2)){
-    $ptstotal = number_format($ptstotal,2);
-    echo '<tr><td>'.'N°'.$n.'</td><td><b>'.$resultatlevel2[$k].'</b></td><td><b>'.$resultatlevel2[$k].'</b> coups -------------- <b>'.$ptstotal.'</b> pts '.'</td></tr>';
+    $ptstotal = number_format($ptstotal,1);
+    // echo '<tr><td>'.'N°'.$n.'</td><td><b>'.$resultatlevel2[$k].'</b></td><td><b>'.$resultatlevel2[$k].'</b> coups -------------- <b>'.$ptstotal.'</b> pts '.'</td></tr>';
     ++$k;
     ++$n;
 }
