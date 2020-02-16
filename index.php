6b0baa3e5adf294d86faccd26b2f0efeb9a37bee
Bouchakour <amine.bouchakour@laplateforme.io>
2020-02-16 02:28:24 +0100

    debut de la fusion des codes d'étienne et amine pour commencer a creer lien entre variable jeux et calcul tableau affichage

---
+++ index.php
@@ -0,0 +1,280 @@
+<?php
+	session_start();
+	if(isset($_GET['deco']))
+  {
+    session_destroy();
+    header("Location: index.php");
+  } 
+class tabcarte
+{
+  public $nbfam;//nombre de famille de carte a retrouver
+  private $nbcar;//nb total de carte
+  private $tabcar;//le tab
+  private $tabcopie;//copie du tab d'avant que le premier coup soit jouer
+  private $nbtour=0;// quel tour
+  public $nbtentative=0;//nb de tentative
+  private $stock;// carte retenu pour deuxieme tour
+  public  $refresh=false;//var reactualisation
+  private $cartrouve=0;
+  public $temp; //retient le temp unix du debut de partie
+ function __construct($nb)
+    {
+    	if($nb>12)
+    	{
+    		$nb=12;
+    	}
+    	if($nb<3)
+    	{
+    		$nb=3;
+    	}
+        $this-> nbfam = $nb;//nombre de famille de carte a retrouver
+        $this-> nbcar = $nb*2;//nb total de carte //crée un tab random chiffre
+        $arr=array();
+		for($i=0; $i < $this->nbfam ; $i++) 
+		{
+			$arr[$i] = $i; 
+		}
+		$arr = array_merge($arr,$arr);
+		shuffle($arr);
+		$this-> arr=$arr; //affichage du tableau       
+        $t="<section>";
+        	for ($j=0; $j < $nb*2 ; $j++)
+        	{	$ch=$j;
+        		
+        		$t=$t."<div class=\"div-carte noturn\" ><a href=\"index.php?jouer=$ch\"><img src=\"img/retourner.png\"></a></div>";//   trouver un moyen de remplire aléatoirment le tab,utiliser un  compteur 
+        	}        	
+        $t=$t."</section>";
+        $this-> tabcar=$t;
+        $_SESSION['gamestrart']=true;
+        $this->temp = time();
+    }
+  public function afficher()
+  {
+    echo $this->tabcar;// afficher le tab 
+  }
+  public function jouer($coup)
+  {	
+  	if($this-> nbtour%2==0)
+  	{
+  		$this-> stock=$coup;//modifier tabcar //crée une variable de copy du dernier tab $tabcopie
+  		$this->tabcopie=$this->tabcar; //quand carte retourer ne pas pouvoir jouer (sup lien )
+  		$cherche = "<div class=\"div-carte noturn\" ><a href=\"index.php?jouer=$coup\"><img src=\"img/retourner.png\"></a></div>";
+		  $remplace ="<div class=\"div-carte turn\" ><img src=\"img/".$this-> arr[$coup].".png\"></div>";
+		  $this->tabcar = str_replace($cherche,$remplace,$this->tabcar);
+      //mise a jour corec bug
+      header("Location: index.php");
+ 
+  		
+  	}
+  	else
+  	{	
+  		
+  	$cherche = "<div class=\"div-carte noturn\" ><a href=\"index.php?jouer=$coup\"><img src=\"img/retourner.png\"></a></div>";
+		$remplace ="<div class=\"div-carte turn\" ><img src=\"img/".$this-> arr[$coup].".png\"></div>";
+		$this->tabcar = str_replace($cherche,$remplace,$this->tabcar);
+  		if ($this-> arr[$this-> stock]==$this-> arr[$coup]) 
+  		{
+  			 $this->cartrouve++;
+          //mise a jour corec bug
+         header("Location: index.php");
+  		}
+  		else
+  		{
+  			$this-> nbtentative++;		
+        header('Location: index.php?chrono=t');//annuler les changement
+  			
+  		}  	//comparaison et modification du tableau
+  	}
+  	$this-> nbtour++;
+
+  	//afficher le coup (trouver un moyen de modiffier le table tabcar ) foreach //tour doit etres un tab qui indique si 1er ou deuxieme coup si deuxiem coup valeur du premeir coup 	//empecher de piocher deux fois de suitte la meme carte
+  }
+  public function tabprecedent()
+  {
+  	$this->tabcar=$this->tabcopie;
+  }
+  public function win()
+  {
+    if($this->cartrouve==$this->nbfam)
+    {
+      $score['tentative']=$this->nbtentative;
+      $score['niv']=$this->nbfam;
+      $tempwin=time();
+      $score['temps']=$tempwin-$this->temp;
+      $_SESSION['score']=$score;
+      
+      $_SESSION['temps']= $score['temps']=$tempwin-$this->temp; //chrono
+      $_SESSION['level']=$score['niv']=$this->nbfam; // level
+      $_SESSION['nb_tentative']=$score['tentative']=$this->nbtentative; // nb-tentative
+
+      if(isset($_POST['time'])){
+          echo 'defi time'.'<br/>';
+      }
+
+      if(isset($_POST['defi'])){
+          echo 'défi tentative'.'<br/>';
+      }
+
+        $connexion=mysqli_connect('localhost',"root","","memory");
+        $requete="INSERT INTO besttime (login,temps,points,level,defi,utilise,id_utilisateur) VALUES ('".$_SESSION['login']."','".$_SESSION['temps']."',Null,'".$_SESSION['level']."','".$_SESSION['defi']."','oui','10') ";
+        $query=mysqli_query($connexion,$requete);
+        echo ($requete).'<br/>';
+
+        $connexion=mysqli_connect('localhost',"root","","memory");
+        $requete="INSERT INTO besttentative (login,nb_tentative,points,level,defi,utilise,id_utilisateur) VALUES ('".$_SESSION['login']."','".$_SESSION['nb_tentative']."',Null,'".$_SESSION['level']."','".$_SESSION['defi']."','oui','10') ";
+        $query=mysqli_query($connexion,$requete);
+        echo ($requete).'<br/>';
+
+     
+
+      ?> 
+      <h1 id="win">Winner</h1> 
+      <a href="partage.php">Partager mon score</a>
+      <p><?=$score['temps']; $_SESSION['chrono']=$score['temps']; ?> secondes</p>            <!--  /* VARIABLE TEMPS A RECUPERER POUR WALL OF FAME*/k ****************************************** -->
+      <?php  //lancé ajout score, nouvelle partie
+    }
+  }
+}
+		       
+/*
+	crée un tableau aléatoir qui sert de bd
+	afficher un tab qui sert uniqument a cliquer
+	trouver un moyen de les comparer
+	retourner carte {si premier carte->tirer deuxieme; si deuxieme carte->(si juste ->laisser carte tourné ,si faux -> chrono + reactalisation des 2 cartes)}
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php
+		if(isset($_GET['chrono'])) //refresh la page toute les 1s
+		{    
+			?>
+				<meta http-equiv="refresh" content="1 ;URL=index.php">
+			<?php 
+		} 
+	?>
+</head>
+<body>
+
+
+<?php 
+include('header.php');
+
+if(isset($_SESSION['gamestrart']))
+{
+	if(isset($_GET['jouer']))
+	{
+		$_SESSION['jeu']->jouer($_GET['jouer']);
+	}
+	$_SESSION['jeu']->afficher();//affiche nb fammille a trouver et nb carte total
+  $_SESSION['jeu']->win();
+  ?><p> Nombre d'erreur : <?php echo $_SESSION['jeu']->nbtentative; $_SESSION['nb_tentative']=$_SESSION['jeu']->nbtentative;?></p>     <!-- VARIABLE NOMBRE DE TENTATIVE A RECUPERER POUR AFFICHAGE WALL OF FAME ******************************************* -->
+  <a href="index.php?end=true">Ressayer</a>
+  <a href="index.php?chang=true">Changer difficulter/mode de jeu</a>
+
+  <?php
+}
+else
+{
+    //formulaire de lancement avec choix et if du post
+  ?>
+  <form method="post" action="index.php">
+    <label>Nombre de carte</label>
+    <input type="number"  name="niv" placeholder="3" step="1" min="3" max="12" ><br>
+    <label>Mode de jeu</label>
+    <select name="defi" id="">
+        <option type="text" name="time">Chrono<br>
+        <option type="text" name="tentative">Sans faute<br>
+    </select>
+    <input type="submit" name="envniv" value="jouer">
+  </form>
+  <?php
+  if(isset($_POST['envniv']))
+  {
+    $_SESSION['jeu']= new tabcarte($_POST['niv']);
+    header("Location: index.php");
+  }
+    
+}
+if(isset($_POST['defi'])=='time'){
+    $_SESSION['defi']=$_POST['defi'];
+}
+echo $_SESSION['defi'].'<br/>';
+
+if(isset($_POST['defi'])=='tentative'){
+    $_SESSION['defi']=$_POST['defi'];
+}
+
+
+	
+?>
+
+</body>
+</html>
+<?php
+if(isset($_GET['chrono']))
+    {    
+      $_SESSION['jeu']->tabprecedent();
+    }
+if(isset($_GET['end']))
+  {
+    $niv=$_SESSION['jeu']->nbfam;
+    unset($_SESSION['jeu']);
+    $_SESSION['jeu']= new tabcarte($niv);
+    header("Location: index.php");
+  }
+if(isset($_GET['chang']))
+  {
+    unset($_SESSION['gamestrart']);
+    header("Location: index.php");
+  }  
+
+?>
+
+
+<style type="text/css">
+  section
+  {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    height: 70vh;
+    align-items: center;
+  }
+  .div-carte
+  {
+    border-radius: 10%;
+    margin: 1%;
+    width: 10%;
+    height: 30%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color:#ce8484;
+    box-shadow: 3px 3px 20px #6a4040;
+    transition: transform;
+    transition-duration: 0.8s;
+
+  }
+  .noturn:hover
+  {
+    transform:  rotateX(-65deg) rotateY(-47deg) rotateZ(-22deg) scale(1.1);
+  }
+  .turn:hover
+  {
+    transform: scale(1.1);
+  }
+  img
+  {
+    width: 80%;
+    margin: auto;
+  }
+  a
+  {
+    align-items: center;
+    display: flex;
+    width: 80%;
+  }
+</style>
